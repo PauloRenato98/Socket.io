6f763acb8332bdc0ab40b1e71a426135cb0b03e4
PauloRenato98 <paulo__159@hotmail.com>
2020-05-03 23:47:16 -0300

    testando aplicação

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
+  console.log("teste");
   res.sendFile(__dirname + '/public/index.html');
 });
 
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   console.log('a user connected' + " " + socket.id);
   socket.on('disconnect', () => {
     console.log('a user disconectado' + " " + socket.id);
@@ -21,15 +22,20 @@ io.emit('some event', {
 }); // This will emit the event to all connected sockets
 
 
-io.on('connection', (socket) => {
- 
-  socket.on('chat message', (msg) => {
+io.on('connection', socket => {
+  socket.on('chat message', msg => {
     console.log(msg)
     io.emit('chat message', msg);
   });
 });
 
 
-http.listen(3000, () => {
+/*http.listen(3000, () => {
   console.log('listening on *:3000');
-});
+});*/
+
+var server_port = process.env.YOUR_PORT || process.env.PORT ||  80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port);
+
+console.log('Listening on port %d', server_port);
